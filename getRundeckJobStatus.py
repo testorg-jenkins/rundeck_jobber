eb7bf0795b5985782d71d64c471a5ba00198c164
Ramanathan Muthiah <rus.cahimb@gmail.com>
2017-01-04 21:01:45 +0530

    trial 10: start rundeck job and get it's status

--- getRundeckJobStatus.py
+++ getRundeckJobStatus.py
@@ -14,10 +14,6 @@ def getJobStatus(rd_project, job_uuid):
 
     rdcmd = rd_cli + "executions query  -p " + rd_project + " -i " + job_uuid + outformat + " -m 1 "
 
-    print(" ==> Using env settings for Rundeck CLI: ")
-    os.system('echo $RD_URL')
-    os.system('echo $RD_TOKEN')
-
     rdcmd_out = subprocess.check_output([rdcmd], shell=True, stderr=subprocess.STDOUT)
 
     ## expected output sample : 'succeeded'
@@ -26,6 +22,9 @@ def getJobStatus(rd_project, job_uuid):
     print()
     print(" ==> Status of latest run for job with UUID, {0}, in Rundeck project, {1} : {2}".format(job_uuid, rd_project, rdjob_status))
 
+    with open('jobinfo.txt', 'w') as rdjob:
+        rdjob.write(rdjob_status)
+
     return None
 
 
@@ -33,6 +32,10 @@ def main():
 
     project = sys.argv[1]
     jobid   = sys.argv[2]
+
+    print(" ==> Using env settings for Rundeck CLI: ")
+    os.system('echo $RD_URL')
+    os.system('echo $RD_TOKEN')
 
     getJobStatus(project, jobid)
 
